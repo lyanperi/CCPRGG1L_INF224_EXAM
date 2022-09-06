84498b7c9cef16f2a24e8d7d5361aef9b16e617a
Lyan Peregrino <111546577+lyanperi@users.noreply.github.com>
2022-09-06 09:45:05 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+    Student Me = new Student();
+    Me.surname = "Peregrino";
+    Me.firstName = "Lyan Francez";
+    Me.middleInitial = "Cataluña";
+    Me.dateOfBirth = 01/19/03;
+    Me.studentNumber = 2022104936;
+    Me.emailAddress = "peregrinolc@students.national-u.edu.ph";
+    Me.iAmAwesome = true;
+    Me.sayMystudentNumber();
+    Me.sayMyEmailAddress();
+    Me.AmIAwesome();
+    }
+    }
